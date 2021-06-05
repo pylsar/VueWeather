11c7844cbf6e0d47f8e3b1855c381ce438fe4a9a
Alexey Kusov <kysov2006@bk.ru>
2021-06-05 21:52:33 +0300

    london baby

--- src/store/index.js
+++ src/store/index.js
@@ -16,19 +16,19 @@ export default new Vuex.Store({
   actions: {
     GET_WEATHER_FROM_API({ commit }) {
       const API_KEY = '9f2f956728142e52c2ecac42c68843be';
-      // const API_CITY ="London";
-      const API_LAT = '51.5';
-      const API_LON = '0.12';
-      // const url = `https://api.openweathermap.org/data/2.5/weather?q=${API_CITY}&appid=${API_KEY}`;
-      const url = `https://api.openweathermap.org/data/2.5/find?lat=${API_LAT}&lon=${API_LON}&cnt=10&appid=${API_KEY}`;
+      const API_CITY ="London";
+      // const API_LAT = '51.5';
+      // const API_LON = '0.12';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${API_CITY}&appid=${API_KEY}`;
+      // const url = `https://api.openweathermap.org/data/2.5/find?lat=${API_LAT}&lon=${API_LON}&cnt=10&appid=${API_KEY}`;
      
       
       axios(url, {
         method: "GET"
       })
         .then((weather) => {
-          commit("SET_WEATHER_TO_STATE", weather.data.list);
-          console.log(weather.data.list);
+          commit("SET_WEATHER_TO_STATE", weather.data);
+          console.log(weather.data);
           return weather;
         })
         .catch((error) => {
--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,28 +1,40 @@
 <template>
     <div class="weather">
-        <div v-for="item in WEATHER" :key="item.id" class="weather__item">
-            <h2>{{item.name}}</h2>
-            <div class="weather__coords">
-                <span>Долгота:{{item.coord.lon}}</span>
-                <span>Широта:{{item.coord.lat}}</span>
-            </div>
-            <div class="weather__main">
-                <span>Температура:{{item.main.temp}}</span>
-                <span>Давление:{{item.main.pressure}}</span>
-            </div>
-
+        
+        <h2>{{WEATHER.name}}</h2>
+        <div class="weather__coord">
+            <span>{{WEATHER.coord.lat}}</span>
+            <span>{{WEATHER.coord.lon}}</span>
         </div>
+        <div class="weather__main">
+            <span>Ощущения: {{WEATHER.main.feels_like}}</span>
+            <span>Давление:{{WEATHER.main.pressure}}</span>
+            <span>Температура:{{WEATHER.main.temp}}</span>
+            <span>Максимальная Температура:{{WEATHER.main.temp_max}}</span>
+            <span>Минимальная Температура:{{WEATHER.main.temp_min}}</span>
+        </div>
+        <div class="weather__wind">
+            <span>Угол Ветра: {{WEATHER.wind.deg}}</span>
+            <span>Скорость Ветра: {{WEATHER.wind.speed}}</span>
+        </div>
+
     </div>
 </template>
 <script>
 import { mapActions, mapGetters } from "vuex";
 export default{
     name: 'Weather',
+    data(){
+        return{
+        }
+    },
     computed: {
         ...mapGetters(["WEATHER"]),
+        
     },
     methods:{
-        ...mapActions(["GET_WEATHER_FROM_API"])
+        ...mapActions(["GET_WEATHER_FROM_API"]),
+        
     },
     mounted() {
         this.GET_WEATHER_FROM_API();
@@ -30,12 +42,6 @@ export default{
 }
 </script>  
 <style lang="scss">
-.weather{
-    display: flex;
-    flex-wrap: wrap;
-    &__item{
-        width: 50%;
-        border: 1px solid red;
-    }
-}
+
+
 </style>
