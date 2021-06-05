9c0663b13b6fc5888d19c3623c71e922df8c3cde
pylsar <kysov2006@bk.ru>
2021-06-05 08:24:06 +0300

    getting data from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    weather: []
   },
   mutations: {
+    SET_WEATHER_TO_STATE: (state, weather) => {
+      state.weather = weather;
+    }
   },
   actions: {
+    GET_WEATHER_FROM_API({ commit }) {
+      const API_KEY = '9f2f956728142e52c2ecac42c68843be';
+      const API_CITY ="London";
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${API_CITY}&appid=${API_KEY}`;
+     
+      
+      axios(url, {
+        method: "GET"
+      })
+        .then((weather) => {
+          commit("SET_WEATHER_TO_STATE", weather.data);
+          console.log(weather.data);
+          return weather;
+        })
+        .catch((error) => {
+          console.log(error);
+          return error;
+        });
+    }
+  },
+  getters:{
+    WEATHER(state) {
+      return state.weather;
+    },
   },
   modules: {
   }
---
+++ src/views/Weather.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="weather">
+        1
+        <div>
+            {{WEATHER.name}}
+        </div>
+    </div>
+</template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default{
+    name: 'Weather',
+    computed: {
+        ...mapGetters(["WEATHER"]),
+    },
+    methods:{
+        ...mapActions(["GET_WEATHER_FROM_API"])
+    },
+    mounted() {
+        this.GET_WEATHER_FROM_API();
+    },
+}
+</script>  
