9b244aa8e96a3038779af23cd0d9676b71d5fa56
Alexey Kusov <kysov2006@bk.ru>
2021-06-05 22:48:15 +0300

    css

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -2,21 +2,23 @@
     <div class="weather">
         <h2>{{WEATHER.name}}</h2>
         <div class="weather__coord">
-            <span>Координаты:</span>
+            <span>Координаты: </span>
             <span>{{WEATHER.coord.lat}}</span>
             <span>{{WEATHER.coord.lon}}</span>
         </div>
         <div class="weather__main">
-            <span>Ощущения: {{WEATHER.main.feels_like}}</span>
-            <span>Давление:{{WEATHER.main.pressure}}</span>
-            <span>Температура:{{WEATHER.main.temp}}</span>
-            <span>Максимальная Температура:{{WEATHER.main.temp_max}}</span>
-            <span>Минимальная Температура:{{WEATHER.main.temp_min}}</span>
+            <span>Ощущения: {{WEATHER.main.feels_like | filterDegees}} C°</span>
+            <span>Давление: {{WEATHER.main.pressure}}</span>
+            <span>Температура: {{WEATHER.main.temp | filterDegees}} C°</span>
+            <span>Максимальная Температура: {{WEATHER.main.temp_max | filterDegees}} C°</span>
+            <span>Минимальная Температура: {{WEATHER.main.temp_min | filterDegees}} C°</span>
         </div>
         <div class="weather__wind">
-            <span>Угол Ветра: {{WEATHER.wind.deg}}</span>
-            <span>Скорость Ветра: {{WEATHER.wind.speed}}</span>
+            <span>Угол Ветра: {{WEATHER.wind.deg}} deg</span>
+            <span>Скорость Ветра: {{WEATHER.wind.speed}} m/s</span>
         </div>
+        <img class="weather__phone" src="../assets/img/phone.png" alt="phone">
+        <img class="weather__umbrella" src="../assets/img/umbrella.png" alt="umbrella">
     </div>
 </template>
 <script>
@@ -34,6 +36,13 @@ export default{
     mounted() {
         this.GET_WEATHER_FROM_API();
     },
+    filters: {
+        filterDegees: function (value) {
+          
+            value = value - 273.15;
+            return value.toFixed(2);
+        }
+    }
 }
 </script>  
 <style lang="scss">
@@ -46,8 +55,8 @@ export default{
     z-index: 1;
     transform: translate(-50%,-50%);
     padding: 24px;
-    box-shadow: white 0px 5px 15px;
-    background: white;
+    box-shadow: #42aaff 0px 5px 15px;
+    background: #42aaff;
     & h2{
         text-align: center;
         margin-top: 48px;
@@ -70,6 +79,18 @@ export default{
         flex-direction: column;
         margin-top: 24px;
         margin-bottom: 24px;
+    }
+    &__phone{
+        width: 150px;
+        position: absolute;
+        right: -76px;
+        bottom: -34px;
+    }
+    &__umbrella{
+        width: 150px;
+        position: absolute;
+        left: -76px;
+        top: -34px;
     }
 }
 
