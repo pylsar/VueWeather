23bec4988dfa7506e46bda135bd33bd1ba46283c
Alexey Kusov <kysov2006@bk.ru>
2021-06-05 21:19:25 +0300

    list of cities

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="weather">
-        1
-        <div>
-            {{WEATHER.name}}
+        <div v-for="item in WEATHER" :key="item.id" class="weather__item">
+            <h2>{{item.name}}</h2>
+            <div class="weather__coords">
+                <span>Долгота:{{item.coord.lon}}</span>
+                <span>Широта:{{item.coord.lat}}</span>
+            </div>
+            <div class="weather__main">
+                <span>Температура:{{item.main.temp}}</span>
+                <span>Давление:{{item.main.pressure}}</span>
+            </div>
+
         </div>
     </div>
 </template>
@@ -21,3 +29,13 @@ export default{
     },
 }
 </script>  
+<style lang="scss">
+.weather{
+    display: flex;
+    flex-wrap: wrap;
+    &__item{
+        width: 50%;
+        border: 1px solid red;
+    }
+}
+</style>
