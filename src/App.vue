45599e395b4dbadece196679bf913c79389daa03
Alexey Kusov <kysov2006@bk.ru>
2021-06-05 22:26:50 +0300

    styles

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,11 @@ export default{
 </script>
 
 <style lang="scss">
-
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  background: black;
+}
 </style>
--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="weather">
-        
         <h2>{{WEATHER.name}}</h2>
         <div class="weather__coord">
+            <span>Координаты:</span>
             <span>{{WEATHER.coord.lat}}</span>
             <span>{{WEATHER.coord.lon}}</span>
         </div>
@@ -17,17 +17,12 @@
             <span>Угол Ветра: {{WEATHER.wind.deg}}</span>
             <span>Скорость Ветра: {{WEATHER.wind.speed}}</span>
         </div>
-
     </div>
 </template>
 <script>
 import { mapActions, mapGetters } from "vuex";
 export default{
     name: 'Weather',
-    data(){
-        return{
-        }
-    },
     computed: {
         ...mapGetters(["WEATHER"]),
         
@@ -42,6 +37,40 @@ export default{
 }
 </script>  
 <style lang="scss">
+.weather{
+    width: 420px;
+    height: 500px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 1;
+    transform: translate(-50%,-50%);
+    padding: 24px;
+    box-shadow: white 0px 5px 15px;
+    background: white;
+    & h2{
+        text-align: center;
+        margin-top: 48px;
+        margin-bottom: 48px;
+        text-transform: uppercase;
+        font-size: 48px;
+    }
+    & span {
+        font-size: 20px;
+    }
 
+    &__main{
+        display: flex;
+        flex-direction: column;
+        margin-top: 24px;
+        margin-bottom: 24px;
+    }
+    &__wind{
+        display: flex;
+        flex-direction: column;
+        margin-top: 24px;
+        margin-bottom: 24px;
+    }
+}
 
 </style>
